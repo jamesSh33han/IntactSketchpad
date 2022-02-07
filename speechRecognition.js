937a619bd21c7771b8701f1f71629b06901829bd
James Sheehan <jfsheeha@uvm.edu>
2022-02-07 14:56:16 -0500

    created speechRecognition.js

--- speechRecognition.js
+++ speechRecognition.js
@@ -5,10 +5,10 @@ var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
 var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
 
 // Defining grammar that we want our application to recognize
-var fileSpeech = [ 'save', 'delete'];
+var fileSpeech = [ 'download', 'delete'];
 var grammar = '#JSGF V1.0; grammar fileSpeech; public <fileSpeech> = ' + fileSpeech.join(' | ') + ' ;'
 
-// Plugging grammar into speech recognition
+// Creating a new SpeechRecognition object instance using SpeechRecognition() constructor
 var recognition = new SpeechRecognition();
 // Define variable to hold the new speech grammar list
 var speechRecognitionList = new SpeechGrammarList();
@@ -21,22 +21,25 @@ recognition.lang = 'en-US';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
-var diagnostic = document.querySelector('.output');
-var bg = document.querySelector('html');
-var hints = document.querySelector('.hints');
+//var diagnostic = document.querySelector('.output');
+//var bg = document.querySelector('html');
+var imagesrc = document.querySelector("#img-file");
 
-var colorHTML= '';
-colors.forEach(function(v, i, a){
+var actionHTML= '';
+fileSpeech.forEach(function(v, i, a){
   console.log(v, i);
-  colorHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
+  actionHTML += '<span style="background-color:' + v + ';"> ' + v + ' </span>';
 });
-hints.innerHTML = 'Tap/click then say a color to change the background color of the app. Try ' + colorHTML + '.';
 
+// set recognition service to start when body of webpage is clicked
 document.body.onclick = function() {
   recognition.start();
-  console.log('Ready to receive a color command.');
+  // display message that console is ready for voice command
+  console.log('Ready to receive an action command.');
 }
 
+// when a result is successfully recognized recognition.onresult handler fires
+// extracts the color that was spoken from the event object and sets background color to that color
 recognition.onresult = function(event) {
   // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
   // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
@@ -46,20 +49,23 @@ recognition.onresult = function(event) {
   // These also have getters so they can be accessed like arrays.
   // The second [0] returns the SpeechRecognitionAlternative at position 0.
   // We then return the transcript property of the SpeechRecognitionAlternative object
-  var color = event.results[0][0].transcript;
-  diagnostic.textContent = 'Result received: ' + color + '.';
-  bg.style.backgroundColor = color;
+  var action = event.results[0][0].transcript;
+  diagnostic.textContent = 'Result received: ' + action + '.';
+  //bg.style.backgroundColor = action;
   console.log('Confidence: ' + event.results[0][0].confidence);
 }
 
+// sets recognition to stop when speech input can no longer be detected
 recognition.onspeechend = function() {
   recognition.stop();
 }
 
+// display message if recognition was unable to match speech to command
 recognition.onnomatch = function(event) {
-  diagnostic.textContent = "I didn't recognise that color.";
+  diagnostic.textContent = "I didn't recognise that command.";
 }
 
+// display message if error occured in recognition
 recognition.onerror = function(event) {
   diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
 }
